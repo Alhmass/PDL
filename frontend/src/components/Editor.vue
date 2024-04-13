ed6c38da6261a0980ad9350329881ca791cfd56c
Marie Sanson <marie.sanson@u-bordeaux.fr>
2024-04-13 14:44:01 +0200

    [Fix]: editor.vue
    fix request for color filter

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -57,12 +57,19 @@ async function imageUploaded(newImage: File) {
   }
 }
 
+function parse(num: number) {
+  let res = "";
+  if(num < 0) res = res.concat("-");
+  return res.concat(Math.abs(num).toString().padStart(3, '0'));
+}
+
 function applyFilter() {
   if (imageToDisplay.value) {
     let numbers;
     if (selectedFilter.value === "colors") {
-      const formattedNumbers = range.value.map(num => num.toString().padStart(3, '0'));
+      const formattedNumbers = range.value.map(num => parse(num));
       numbers = formattedNumbers.join('');
+      console.log(numbers);
     } else
       numbers = range.value.join('');
     api.getImageFilter(imageToDisplay.value.id, selectedFilter.value, numbers).then((data) => {
@@ -81,7 +88,7 @@ function resetRange() {
   if (selectedFilter.value === "GrayLevel")
     range.value = []
   else if (selectedFilter.value === "colors")
-    range.value = [-255, -255, -255];
+    range.value = [0, 0, 0];
   else if (selectedFilter.value === "mean")
     range.value = [1];
   else
@@ -118,17 +125,17 @@ function resetRange() {
         <div v-if='selectedFilter === "colors"' class="sliderColors">
           <div>
             <span class="value-display" id="redValueDisplay">{{ range[0] }}</span>
-            <input v-model="range[0]" type="range" id="redRange" min="-255" max="255" value="-255">
+            <input v-model="range[0]" type="range" id="redRange" min="-255" max="255" value="0">
             <label for="redRange">R</label>
           </div>
           <div>
             <span class="value-display" id="greenValueDisplay">{{ range[1] }}</span>
-            <input v-model="range[1]" type="range" id="greenRange" min="-255" max="255" value="-255">
+            <input v-model="range[1]" type="range" id="greenRange" min="-255" max="255" value="0">
             <label for="greenRange">G</label>
           </div>
           <div>
             <span class="value-display" id="blueValueDisplay">{{ range[2] }}</span>
-            <input v-model="range[2]" type="range" id="blueRange" min="-255" max="255" value="-255">
+            <input v-model="range[2]" type="range" id="blueRange" min="-255" max="255" value="0">
             <label for="blueRange">B</label>
           </div>
         </div>
@@ -142,7 +149,7 @@ function resetRange() {
         <div v-else-if='selectedFilter === "mean"' class="brightSlider">
           <div>
             <span class="value-display" id="meanValueDisplay">{{ range[0] }}</span>
-            <input v-model="range[0]" type="range" id="meanSlider" name="meanSlider" min="1" max="51" step="2"
+            <input v-model="range[0]" type="range" id="meanSlider" name="meanSlider" min="1" max="21" step="2"
               value="1" />
             <label for="meanSlider">{{ selectedFilter }}</label>
           </div>
